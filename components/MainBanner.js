6f73558d36ae36cf5032d0c30d57cc6d0ac32c77
Nomen Ama <83960342+nomenama@users.noreply.github.com>
2021-08-31 15:11:14 +0100

    add text animation to main banner

--- components/MainBanner.js
+++ components/MainBanner.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import Column from "./Column";
 import Row from "./Row";
+import Typed from "typed.js";
 
 const MainBanner = () => {
+
+	const el = useRef(null);
+
+	useEffect(() => {
+		const typed = new Typed(el.current, {
+			strings: ["Software Developer", "UI/UX Designer", "JavaScript Programmer"],
+			typeSpeed: 150,
+			backSpeed: 60,
+			loop: true
+		});
+
+		return () => {
+			typed.destroy();
+		};
+	}, []);
 
 	return (
 		<section className="home" id="home">
@@ -10,12 +26,12 @@ const MainBanner = () => {
 				<Row className="homeContent">
 					<h3>Hello, my name is</h3>
 					<h1>Nomen Ama</h1>
-					<h2>And I am a <span>Developer</span></h2>
+					<h2>And I am a <span ref={el}/></h2>
 					<a href="#">Hire Me</a>
 				</Row>
 			</Column>
 		</section>
-	)
-}
+	);
+};
 
 export default MainBanner;
