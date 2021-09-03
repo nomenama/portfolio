f834c623c51332b10d0acd7ef9ec62e0607e4b77
Nomen Ama <83960342+nomenama@users.noreply.github.com>
2021-09-04 00:31:15 +0100

    import Typed and add typing interaction

--- components/About.js
+++ components/About.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import Column from "./Column";
 import Row from "./Row";
+import Typed from "typed.js";
 
 const About = () => {
+
+	const el = useRef(null);
+
+	useEffect(() => {
+
+		const typed = new Typed(el.current, {
+			strings: ["developer", "thinker","designer"],
+			typeSpeed: 150,
+			backSpeed: 100,
+			loop: true
+		});
+
+		return () => {
+			typed.destroy();
+		};
+	}, []);
 
 	return (
 		<section className="about" id="about">
@@ -13,10 +30,10 @@ const About = () => {
 
 				<Row className="aboutContent">
 
-						<img src="/assets/profile-1.jpeg" alt="image of the author"/>
+						<img src={"/assets/profile-1.jpeg"} alt="image of the author"/>
 
 					<div className="detailColumn">
-						<div className="text">I&apos;m Nomen and I&apos;m a <span>Developer</span></div>
+						<div className="text">I&apos;m Nomen and I&apos;m a <span ref={el}/></div>
 						<p>
 							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam blanditiis commodi corporis dolores dolorum eum excepturi impedit ipsam, magni, molestiae nam nobis nostrum officiis quasi quisquam rem sit tempore ullam ut veritatis! Accusantium, aperiam aspernatur assumenda consequuntur, dignissimos dolor doloribus ducimus est explicabo id magnam modi neque numquam rerum ullam.
 						</p>
