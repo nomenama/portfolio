70f6b1c6cdf5de2614af5e6dcf765443eb69a164
nomen <justnomen@outlook.com>
2023-07-03 14:01:52 +0100

    re-initialise new portfolio

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-5">
+      <h1>HELLO WORLDS</h1>
+    </main>
+  )
+}
---
+++ app/works/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Page = (props) => {
+	return (
+		<main>
+			<h1>Works page</h1>
+			<p>This is where all the past project lies</p>
+		</main>
+	)
+}
+
+export default Page;
