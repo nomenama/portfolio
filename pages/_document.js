df5656da94749479cc41be5f6bfd6b4fa8791ca0
nomen <justnomen@outlook.com>
2024-04-12 12:02:48 +0100

    update dependencies

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
             <Head />
                 <link rel="shortcut icon" href="/assets/img/favicon.ico" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet" />
             <body>
                 <Main />
